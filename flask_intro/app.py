c03682a1ca473b289692d46936c329896da579dc
arnaud-coding <arnaudberthollet@hotmail.fr>
2025-09-05 14:34:24 +0200

    format setttings

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def welcome():
-    return render_template('welcome.html')
+    return render_template("welcome.html")
 
-@app.route('/bonjour/<string:name>')
+
+@app.route("/bonjour/<string:name>")
 def bonjour(name: str):
-    return render_template('bonjour.html', name=name)
+    return render_template("bonjour.html", name=name)
 
-@app.route('/devices')
+
+@app.route("/devices")
 def devices():
-    return render_template('devices.html')
+    return render_template("devices.html")
+
+
+# simple server-side redirect example
+@app.route("/goto-devices")
+def goto_devices():
+    return redirect(url_for("devices"))
+
+
+# ...existing code...
