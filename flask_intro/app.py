d1378c0f6e39701b689bc52a660c242d9e47ae6b
arnaud-coding <arnaudberthollet@hotmail.fr>
2025-09-12 19:48:27 +0200

    add first device with SQLAlchemy

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,9 +7,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 
 
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    ip = db.Column(db.String(120), unique=True, nullable=False)
+    status = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<Device {self.name}>"
+
+
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
@@ -21,9 +31,23 @@ def hello(name: str):
     return render_template("hello.html", name=name)
 
 
-@app.route("/devices")
+@app.route("/devices", methods=["POST", "GET"])
 def devices():
-    return render_template("devices.html")
+    if request.method == "POST":
+        device_name = request.form["name"]
+        device_ip = request.form["ip"]
+        device_status = request.form["status"]
+        new_device = Device(name=device_name, ip=device_ip, status=device_status)
+
+        try:
+            db.session.add(new_device)
+            db.session.commit()
+            return redirect("/devices")
+        except:
+            return "There was an issue adding your device"
+    else:
+        devices = Device.query.order_by(Device.id).all()
+        return render_template("devices.html", devices=devices)
 
 
 @app.route("/device-data")
@@ -35,3 +59,7 @@ def device_data():
 @app.route("/goto-devices")
 def goto_devices():
     return redirect(url_for("devices"))
+
+@app.route("/addForm")
+def addForm():
+    return render_template("addForm.html")
---
+++ flask_intro/docs/personnal-notes.md
@@ -0,0 +1,12 @@
+# TODO
+
+- Faire marcher la nav avec les boutons
+- Afficher un menu de navigation
+- Définir une classe `Device` avec 3 propriétés : `name`, `ip`, `status`
+- Dans cette page :
+  - Créer une instance de `Device` pour Diamond Loïc
+  - Afficher cet objet en utilisant un composant `Device_line`
+- Page pour afficher des Pokémon (via API)
+- Backend API qui renvoie des données hardcodées
+- Utiliser un fichier JSON au lieu des données codées
+- Remplacer par SQLAlchemy plus tard
